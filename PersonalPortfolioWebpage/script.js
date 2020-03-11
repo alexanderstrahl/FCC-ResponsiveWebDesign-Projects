9a1dada08b391dc3e6053503e3a83588d65f05de
astrahl008 <astra008@plattsburgh.edu>
2020-03-11 14:11:12 -0400

    Playing around with fun mode animations

--- PersonalPortfolioWebpage/script.js
+++ PersonalPortfolioWebpage/script.js
@@ -1,6 +1,12 @@
 console.log("Welcome to my portfolio Website!");
 
 function darkMode() {
-  var element = document.body;
+  let element = document.body;
   element.classList.toggle("dark-mode");
+}
+function psychicTrip() {
+  var psychic = document.body;
+  // for(let i=0;i<100;i++){
+    psychic.classList.toggle("psychic-trip");
+
 }
